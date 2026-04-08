bddb1aa78474602074bb47fa4e23c3a2a21d445f
Aparna Mounika Anupoju <manupoju@msa.miraclesoft.com>
2026-04-08 13:50:05 +0530

    Initial Selenium project

---
+++ tests/test_app.py
@@ -0,0 +1,21 @@
+import subprocess
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def test_exe_web_app():
+    # Start EXE
+    process = subprocess.Popen(["dist/main.exe"])
+
+    time.sleep(5)  # wait for server
+
+    options = Options()
+    options.add_argument("--headless=new")
+
+    driver = webdriver.Chrome(options=options)
+    driver.get("http://127.0.0.1:5000")
+
+    assert "Hello from EXE App!" in driver.page_source
+
+    driver.quit()
+    process.terminate()
