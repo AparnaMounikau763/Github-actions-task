854206660893b18ccf96121d55ae752b7d3e92b0
AparnaMounikau763 <105029885+AparnaMounikau763@users.noreply.github.com>
2026-04-08 15:24:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from EXE App!"
+    return "Hello, This is a Test Applications"
 
 if __name__ == "__main__":
     app.run(port=5000)
